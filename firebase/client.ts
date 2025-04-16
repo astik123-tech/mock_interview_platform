9d2b2871468fa4a1e08049df53f342858e683cdf
astik123-tech <astiksarathe575@gmail.com>
2025-04-16 14:50:04 +0530

    Error fix

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,20 +1,26 @@
+// Import the functions you need from the SDKs you need
 import { getApp, getApps, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLWExObauoY0drQwquJ0VFrGAC-pK6vMQ",
-  authDomain: "mock-interview-platform-37757.firebaseapp.com",
-  projectId: "mock-interview-platform-37757",
-  storageBucket: "mock-interview-platform-37757.firebasestorage.app",
-  messagingSenderId: "331119930991",
-  appId: "1:331119930991:web:e7e8abd923b1d66f89a79a",
-  measurementId: "G-M28GGETMC0"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
